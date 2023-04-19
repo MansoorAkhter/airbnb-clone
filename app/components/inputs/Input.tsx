ccfb6f47c7d1318f5194451a06869853a6933757
Mansoor Akhter <mansoor21st@Gmail.com>
2023-04-19 16:06:58 +0500

    modal form...

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -1,5 +1,7 @@
 'use client'
+
 import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
+import { BiDollar } from "react-icons/bi";
 
 interface InputProps {
     id: string,
@@ -24,7 +26,14 @@ const Input: React.FC<InputProps> = ({
 
     return (
         <div className="w-full relative">
-
+            {formatePrice && (<BiDollar size={24} className="text-neutral-700 w-full absolute top-5 left-2" />)}
+            <input
+                placeholder=" "
+                disabled={disabled}
+                {...register(id, { required })}
+                type={type}
+                className={`peer w-full p-4 pt-6 font-light rounded-md bg-white border-2`}
+            />
         </div>
     );
 }
